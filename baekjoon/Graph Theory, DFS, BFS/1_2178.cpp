bec661f81b451851c8d471a8b98bc2737bc6413b
gyunjongpark <85558481+gyunjongpark@users.noreply.github.com>
2024-03-07 21:38:22 +0900

    Add files via upload

---
+++ baekjoon/Graph Theory, DFS, BFS/1_2178.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m;
+const int dy[]={-1,0,1,0};
+const int dx[]={0,1,0,-1};
+int a[104][104],visited[104][104];
+
+void bfs(int y,int x){
+	visited[y][x]=1;
+	queue<pair<int,int>> q;
+	q.push({y,x});
+	
+	while(q.size()){
+		tie(y,x) = q.front(); q.pop();
+		
+		for(int i=0;i<4;i++){
+			int ny=y+dy[i];
+			int nx=x+dx[i];
+			
+			if(ny<0 || ny>=n || nx<0 || nx>=m) continue;
+			
+			if(a[ny][nx] && !visited[ny][nx]){
+				visited[ny][nx] = visited[y][x]+1;
+				q.push({ny,nx});
+			}
+		}
+	}
+	return;
+}
+
+int main(){
+	
+	cin>>n>>m;
+	
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			scanf("%1d",&a[i][j]);
+	
+	bfs(0,0);
+	
+	cout<<visited[n-1][m-1];
+	
+	return 0;
+}
