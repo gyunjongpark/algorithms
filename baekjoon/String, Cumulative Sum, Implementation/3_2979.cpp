ede2010313ab1626e3f8322be8328a3c266c0826
gyunjongpark <85558481+gyunjongpark@users.noreply.github.com>
2024-03-07 22:26:40 +0900

    Create 3_2979.cpp

---
+++ baekjoon/String, Cumulative Sum, Implementation/3_2979.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum=0;
+int cnt[104];
+int a,b,c,d,e;
+
+int main(){
+	cin>>a>>b>>c;
+	
+	for(int i=0;i<3;i++){
+		
+		cin>>d>>e;
+		
+		for(int i=d;i<e;i++){
+			cnt[i]++;
+		}
+		
+	}
+	
+	for(int i=1;i<=100;i++){
+		if(cnt[i]==1) sum+=a;
+		else if(cnt[i]==2) sum+=2*b;
+		else if(cnt[i]==3) sum+=3*c;
+	}
+	cout<<sum;
+	return 0;
+}
