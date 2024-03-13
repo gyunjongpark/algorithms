4fdf3f773e0a74f0b436750f754681745c7fbd45
gyunjongpark <85558481+gyunjongpark@users.noreply.github.com>
2024-03-13 20:10:28 +0900

    Update 005_1992.cpp

--- baekjoon/Graph Theory, DFS, BFS/005_1992.cpp
+++ baekjoon/Graph Theory, DFS, BFS/005_1992.cpp
@@ -1,13 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std; 
 typedef long long int ll;     
+
 int n; 
 string s;   
 char a[101][101];
+
 string quard(int y, int x, int size){
     if(size == 1) return string(1, a[y][x]); 
+	
     char b = a[y][x]; 
     string ret = "";  
+	
     for(int i = y; i < y + size; i++){
         for(int j = x; j < x + size; j++){ 
             if(b != a[i][j]){ 
@@ -24,14 +28,16 @@ string quard(int y, int x, int size){
     return string(1, a[y][x]); 
 }
 int main(){
-	ios_base::sync_with_stdio(0); cin.tie(NULL);
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
     cin >> n; 
+	
     for(int i = 0; i < n; i++){
         cin >> s; 
         for(int j = 0; j < n; j++){
             a[i][j] = s[j];
         }
     }
+	
     cout << quard(0, 0, n) << '\n'; 
 	return 0; 
 }
