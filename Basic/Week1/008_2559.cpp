477fb79bf786d1105e09deb0b921d5d265e945ce
gyunjongpark <85558481+gyunjongpark@users.noreply.github.com>
2024-03-26 22:32:37 +0900

    Create 008_2559.cpp

---
+++ Basic/Week1/008_2559.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,k,temp,psum[100004],ret=-10000004;
+
+int main(){
+	
+	cin>>n>>k;
+	
+	for(int i=1;i<=n;i++){
+		cin>>temp;
+		psum[i] = psum[i-1] + temp;
+	}
+	
+	for(int i=k;i<=n;i++) ret = max(ret,psum[i]-psum[i-k]);
+	
+	cout<<ret;
+		
+	return 0;
+}
