ce06330120fcf4915260b6710d96c345c32a1d57
gyunjongpark <85558481+gyunjongpark@users.noreply.github.com>
2024-03-26 22:28:43 +0900

    Create 002_10808.cpp

---
+++ Basic/Week1/002_10808.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string s;
+int cnt[26];
+int main(){
+	
+	cin>>s;
+	
+	for(char c : s) cnt[c-'a']++;
+	
+	for(int i : cnt) cout<<i<<' ';
+	
+	return 0;
+}
